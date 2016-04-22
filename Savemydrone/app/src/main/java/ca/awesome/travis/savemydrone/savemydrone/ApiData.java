694eec694028841aa85af40c597776e35f42b298
running-codebase <info@traviscoulter.ca>
2016-04-22 22:54:49 +1000

    Updated .gitignore and added signatures for no fly zone checkers

--- Savemydrone/app/src/main/java/ca/awesome/travis/savemydrone/savemydrone/ApiData.java
+++ Savemydrone/app/src/main/java/ca/awesome/travis/savemydrone/savemydrone/ApiData.java
@@ -1,7 +1,22 @@
 package ca.awesome.travis.savemydrone.savemydrone;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by tco on 16-04-22.
  */
 public class ApiData {
+
+    public boolean radiusInPolygon(LatLng latLng, double kmRadius, LatLng[] polygon) {
+
+        //
+        
+
+        return false;
+    }
+
+    public boolean pointInRadius(LatLng point1, double radius, LatLng point2) {
+
+        return false;
+    }
 }
