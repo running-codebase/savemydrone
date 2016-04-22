4c5d7399f24b9bbf89dde800401b8707ee46ca59
running-codebase <info@traviscoulter.ca>
2016-04-23 00:32:05 +1000

    Added gps data

--- Savemydrone/app/src/main/java/ca/awesome/travis/savemydrone/savemydrone/MapsActivity.java
+++ Savemydrone/app/src/main/java/ca/awesome/travis/savemydrone/savemydrone/MapsActivity.java
@@ -1,8 +1,17 @@
 package ca.awesome.travis.savemydrone.savemydrone;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -21,53 +30,64 @@ import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private LocationManager locationManager;
+    private LatLng currentLngLat;
     private GoogleMap mMap;
-    private static final int circleRadius = 10000;
+    private static final int circleRadius = 1000000;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        testOverlapAlgorithms();
-
-
-    }
-
-    public void testOverlapAlgorithms(){
-
-
+        initializeLocationManager();
 
     }
 
+    private void initializeLocationManager(){
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                currentLngLat = new LatLng(location.getLatitude(), location.getLongitude());
+                addMarker(currentLngLat, "You are Here");
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        if ( Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
-//        mMap.addCircle()
-
-        LatLng sydney = new LatLng(-34, 151);
-      //  moveToUserGPS();
-
+        LatLng sydney = new LatLng(-10, 151);
+//        moveToUserGPS();
 //        addCircle(sydney, "Sydney", circleRadius, 10);
-        addPolygon(sydney);
+//        addPolygon(sydney);
+//        addMarker(new LatLng(-34, 151), "marker title");
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-//        addMarker(new LatLng(-34, 151), "marker title");
-
-    }
-
-
-
-    private void moveToUserGPS(){
-        // Add a marker in Sydney and move the camera
 
     }
 
@@ -92,7 +112,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private void addPolygon(LatLng latLng){
         Polygon mClickablePolygonWithHoles = mMap.addPolygon(new PolygonOptions()
-                        .addAll(createRectangle(latLng, 5, 5))
+                        .addAll(createPolygon())
 //                .addHole(createRectangle(new LatLng(-22, 128), 1, 1))
 //                .addHole(createRectangle(new LatLng(-18, 133), 0.5, 1.5))
                         .fillColor(Color.CYAN)
@@ -113,15 +133,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 new LatLng(center.latitude + halfHeight, center.longitude - halfWidth),
                 new LatLng(center.latitude - halfHeight, center.longitude - halfWidth));
     }
-//
-//    private List<LatLng> createPolygon() {
-//        return Arrays.asList(
-//                new LatLng(),
-//                new LatLng(),
-//                new LatLng(),
-//                new LatLng());
-//
-//
-//    }
+
+    private List<LatLng> createPolygon() {
+        return Arrays.asList(
+                new LatLng(-34, 155),
+                new LatLng(-39, 160),
+                new LatLng(-45, 151),
+                new LatLng(-39, 143),
+                new LatLng(-20, 127),
+                new LatLng(-10, 151));
+
+
+    }
 
 }
