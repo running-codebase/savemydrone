bf439128e10f16e0997a9d72cd245822e7876bb0
redtedtezza <edward.william.terry@gmail.com>
2016-04-23 15:18:22 +1000

    Inserted algorithms for weather criteria checks

--- Savemydrone/app/src/main/java/ca/awesome/travis/savemydrone/savemydrone/Checklist.java
+++ Savemydrone/app/src/main/java/ca/awesome/travis/savemydrone/savemydrone/Checklist.java
@@ -3,7 +3,60 @@ package ca.awesome.travis.savemydrone.savemydrone;
 /**
  * Created by Edward on 23/04/2016.
  */
-public class Checklist {
+public class ChecklistFragment {
+
+//    Logic for comparing actual steady wind speed and wind gusts to criteria wind speed and gusts
+
+    public void windCheck(double windSteady, double windGust) {
+//      Define steady and gust wind limits
+        double windSteadyLimit = 10; // knots
+        double windGustLimit = 15; // knots
+        boolean windCheck;
+
+//      Determine whether the steady or gust wind limits are exceeded
+        if (windSteady > windSteadyLimit | windGust > windGustLimit) {
+            windCheck = false;
+        } else windCheck = true;
+        return;
+    }
+
+//      Logic for comparing actual cloud base to criteria cloud base
+
+    public void cloudCheck(int cloudBase) {
+//      Define lower cloud base limit
+        int cloudBaseLimit=500; // feet
+        boolean cloudCheck;
+
+//      Determine whether the cloud base limits are exceeded
+        if (cloudBase <= cloudBaseLimit) {
+            cloudCheck = false;
+        } else cloudCheck = true;
+        return;
+    }
+
+    public void daylightCheck(int time) {
+//      Define minimum time before sunset
+        int daylightLimit=30; // minutes before sunset
+        boolean daylightCheck;
+
+//      Determine whether there is sufficient time for estimated flight duration before sunset
+        if (time > daylightLimit) {
+            daylightCheck = false;
+        } else daylightCheck = true;
+        return;
+    }
+
+    public void visibilityCheck(int visibility) {
+//      Define minimum visibility
+        double visibilityLimit=1.5*1.609; // km (1km=1.609 statute miles)
+        boolean visibilityCheck;
+
+//      Determine whether there is sufficient visibility
+        if (visibility < visibilityLimit) {
+            visibilityCheck = false;
+        } else visibilityCheck = true;
+        return;
+    }
 
 //    public void checklistCriteria(boolean airspaceCheck, boolean windCheck, boolean cloudCheck,
 //                                  boolean daylightCheck, boolean visibilityCheck) {
