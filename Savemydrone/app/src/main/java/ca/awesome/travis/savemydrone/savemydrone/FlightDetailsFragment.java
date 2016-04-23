d3dc8a00d87155545c224eb7d5326b7f18103e57
running-codebase <info@traviscoulter.ca>
2016-04-23 13:27:16 +1000

    Flight details fragment

--- Savemydrone/app/src/main/java/ca/awesome/travis/savemydrone/savemydrone/FlightDetailsFragment.java
+++ Savemydrone/app/src/main/java/ca/awesome/travis/savemydrone/savemydrone/FlightDetailsFragment.java
@@ -1,5 +1,6 @@
 package ca.awesome.travis.savemydrone.savemydrone;
 
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -7,7 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 /**
  * Created by tco on 16-04-23.
@@ -16,6 +20,10 @@ public class FlightDetailsFragment extends Fragment {
 
     public static final String TAG = "FlightDetailsFragment";
     private Context context;
+
+    private EditText flightRangeEditText;
+    private EditText flightTimeEditText;
+
 
     public static FlightDetailsFragment newInstance(){
         return new FlightDetailsFragment();
@@ -26,9 +34,8 @@ public class FlightDetailsFragment extends Fragment {
             savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_set_flight_details, container, false);
 
-        EditText flightRangeEditText = (EditText) view.findViewById(R.id.flight_range_editText);
-//        EditText flightEditText = (EditText) view.findViewById(R.id.flight_range_editText);
-
+        flightRangeEditText = (EditText) view.findViewById(R.id.flight_range_editText);
+        flightTimeEditText = (EditText) view.findViewById(R.id.flight_time_editText);
 
 
         Button doneButton = (Button) view.findViewById(R.id.done_button);
@@ -45,6 +52,14 @@ public class FlightDetailsFragment extends Fragment {
 
 
     private void donePressed() {
+        if (!flightRangeEditText.getText().equals("")){
+            ((MapsActivity)getActivity()).sharedPreferences.setFlightRange(Float.parseFloat(flightRangeEditText.getText().toString()));
+        }
+
+        if (!flightTimeEditText.getText().equals("")){
+            ((MapsActivity)getActivity()).sharedPreferences.setFlightTime(Integer.parseInt(flightTimeEditText.getText().toString()));
+        }
+
         ((MapsActivity)getActivity()).doneButtonPressed(this);
     }
 }
